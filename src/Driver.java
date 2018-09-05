c895212b943d6b4f847ecd4d12caada4959b7946
Andrew <Andrew@10.1.121.174>
2018-09-04 20:02:56 -0700

    Tested Line arguments

--- src/Driver.java
+++ src/Driver.java
@@ -12,15 +12,16 @@ public class Driver {
 	 * engine from files or the web.
 	 *
 	 * @param args the command-line arguments to parse
-	 * @return 0 if everything went well
 	 */
-	public static int main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Fill in
 		System.out.println(Arrays.toString(args));
+		String[] validArguments = {"-path", "-index"};
+		if (args.length == 0 || !args[0].equals(validArguments[0]) && !args[0].equals(validArguments[1])) {
+			System.err.println("Command line argument not valid.\nValid arguments: \n-path\n-index");
+		}
 		
 		Path path = Paths.get("..", "project-tests", "text");
-		
-		return 0;
 	}
 
 }
