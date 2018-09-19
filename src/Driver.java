71ba8b39f0f90f633180d4549e8376276b3e2cc9
Andrew Nguyen <secretbetta@gmail.com>
2018-09-18 17:47:36 -0700

    feeling so stressed

--- src/Driver.java
+++ src/Driver.java
@@ -480,13 +480,13 @@ public class Driver {
 		
 		if (argmap.hasFlag("-index") && argmap.getPath("-index") == null) {
 //			index = Paths.get("..", "project-tests", "index.json");
-			index = Paths.get("..", "out", "index.json");
+			index = Paths.get("..", "index.json");
 		} else if (argmap.hasFlag("-index") && !(argmap.getPath("-index") == null)) {
 //			index = Paths.get("..", "project-tests", argmap.getPath("-index").toString());
-			index = Paths.get("..", "out", argmap.getPath("-index").toString());
+			index = Paths.get("..", argmap.getPath("-index").toString());
 		} else {
 //			index = Paths.get("..", "project-tests", "out", "index.json");
-			index = Paths.get("..", "out", "index.json");
+			index = Paths.get("..", "index.json");
 		}
 		
 		writer = Files.newBufferedWriter(index, StandardCharsets.UTF_8);
