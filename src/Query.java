bfd1efccda7e5be2fde669a9490520ee9ca4da69
Andrew Nguyen <secretbetta@gmail.com>
2018-10-10 10:51:11 -0700

    Testing some things in query

--- src/Query.java
+++ src/Query.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 
 /**
  * Stores basic movie information.
@@ -7,7 +8,7 @@ import java.io.StringWriter;
 public class Query {
 
 	private final String word;
-	private final Result result;
+	private final ArrayList<Result> results;
 	
 	/**
 	 * 
@@ -16,22 +17,37 @@ public class Query {
 	 * @param score
 	 * @param count
 	 */
-	public Query(String word, Result result) {
+	public Query(String word, ArrayList<Result> results) {
 		this.word = word;
-		this.result = result;
+		this.results = results;
 	}
 
 	public String word() {
 		return this.word;
 	}
 
-	public Result results() {
-		return this.result;
+	public ArrayList<Result> results() {
+		return this.results;
+	}
+	
+	public void sort() {
+		System.out.println(results.get(0));
+		Result temp;
+		for (int x = 0; x < this.results.size(); x++) {
+			for (int y = x + 1; y < this.results.size(); y++) {
+				if (this.results.get(x).compareTo(this.results.get(y)) == 1) {
+					temp = this.results.get(x);
+					this.results.get(x) = this.results.get(y);
+					this.results.get(y) = temp;
+				}
+			}
+		}
 	}
 
 	public String toString(int level) throws IOException {
 		StringWriter writer = new StringWriter();
 		
+		this.sort();
 		
 		writer.write("[");
 		writer.write(System.lineSeparator());
@@ -51,17 +67,17 @@ public class Query {
 		writer.write(System.lineSeparator());
 		NestedJSON.indent(level + 3, writer);
 		
-//		for (int i = 0; i < results.length; i++) {
-//			writer.write(results[i].toString(0));
-//			if (i != results.length - 1) {
-//				writer.write(",");
-//				writer.write(System.lineSeparator());
-//				NestedJSON.indent(level + 3, writer);
-//			} else {
-//				writer.write(System.lineSeparator());
-//				NestedJSON.indent(level + 2, writer);
-//			}
-//		}
+		for (int i = 0; i < results.size(); i++) {
+			writer.write(results.get(i).toString(0));
+			if (i != results.size() - 1) {
+				writer.write(",");
+				writer.write(System.lineSeparator());
+				NestedJSON.indent(level + 3, writer);
+			} else {
+				writer.write(System.lineSeparator());
+				NestedJSON.indent(level + 2, writer);
+			}
+		}
 		
 		writer.write("]");
 		writer.write(System.lineSeparator());
@@ -71,12 +87,19 @@ public class Query {
 	}
 	
 	public static void main(String[] args) throws IOException {
-//		Result[] result = new Result[1];
+		ArrayList<Result> results = new ArrayList<Result>();
 		Result result = new Result("file.txt", 11, 0.27272727);
-		Query querymap = new Query("word", result);
-//		Path index = Paths.get("..", "testing.json");
+		Result result1 = new Result("secondfile.txt", 13, 0.33333333);
+		results.add(result);
+		results.add(result1);
 		
-		System.out.println(querymap.toString(0));
+//		result.toString(0);
+//		System.out.println();
+		Query querymap = new Query("word", results);
+		
+		querymap.toString(0);
+		
+//		System.out.println(querymap.toString(0));
 		
 	}
 }
