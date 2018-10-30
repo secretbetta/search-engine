ee23afdb767de02c12c76f641192dbc307b50240
Andrew Nguyen <secretbetta@gmail.com>
2018-10-30 09:30:26 -0700

    Creating a new search method

--- src/JSONReader.java
+++ src/JSONReader.java
@@ -84,4 +84,71 @@ public class JSONReader {
 			queries.put(line, results);
 		}
 	}
+	
+	public static void searcher(TreeMap<String, Integer> locationIndex, 
+			TreeMap<String, ArrayList<Result>> queries, 
+			Path path, 
+			InvertedIndex index, 
+			List<String> query, 
+			boolean exact) throws IOException {
+		Result result = null;
+		String filename = path.toString();
+		double wordtotal = 0;
+		int wordcount = 0;
+		
+		ArrayList<Result> results = new ArrayList<Result>();
+		
+		TreeSet<String> temp = new TreeSet<String>();
+		temp.addAll(query);
+		
+		String line = "";
+		for (String w : temp.headSet(temp.last())) {
+			line += w + " ";
+		}
+		line += temp.last();
+		
+		if (exact) {
+			for (String q : temp) {
+				wordtotal = 0;
+//				if (index.containsKey(q) && index.get(q).containsKey(filename)) {
+				if (index.contains(q, filename)) {
+					wordtotal = locationIndex.get(filename);
+					
+					wordcount = index.get(q).get(filename).size() + wordcount;
+					result = new Result(filename, wordcount, ((double)wordcount)/((double)wordtotal));
+				}
+			}
+		} else {
+			for (String q : temp) {
+				wordtotal = 0;
+				
+				wordtotal = locationIndex.containsKey(filename) ? locationIndex.get(filename) : 1;
+				
+//				for (String word : index.keySet()) {
+//					if (word.startsWith(q)) {
+//						if (index.get(word).containsKey(filename)) {
+//							wordcount = index.get(word).get(filename).size() + wordcount;
+//							result = new Result(filename, wordcount, ((double)wordcount)/((double)wordtotal));
+//						}
+//					}
+//				}
+			}
+		}
+		
+		results.add(result);
+		
+		boolean containFlag = false;
+		if (queries.containsKey(line)) {
+			for (Result r : queries.get(line)) {
+				if (result != null && r != null && r.file.equals(result.file)) {
+					containFlag = true;
+				}
+			}
+			if (!containFlag && result != null) {
+				queries.get(line).add(result);
+			}
+		} else {
+			queries.put(line, results);
+		}
+	}
 }
