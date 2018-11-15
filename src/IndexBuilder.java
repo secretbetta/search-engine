793f06a30f0e3bbaf80b9d66d88adeb8baf9ff82
Andrew Nguyen <secretbetta@gmail.com>
2018-11-14 18:54:03 -0800

    In the processing of merging

--- src/IndexBuilder.java
+++ src/IndexBuilder.java
@@ -36,7 +36,7 @@ public class IndexBuilder {
 	 * @param invertedIndex The index to edit
 	 * @throws IOException For BufferedReader
 	 */
-	public static void getWords(Path file, InvertedIndex invertedIndex) throws IOException {
+	public static void getWords(Path file, InvertedIndex index) throws IOException {
 		try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);) {
 			int pos = 0;
 			String line = null;
@@ -46,9 +46,51 @@ public class IndexBuilder {
 			
 			while ((line = reader.readLine()) != null) {
 				for (String word : TextParser.parse(line)) {
-					invertedIndex.add(stem.stem(word).toString(), filename, ++pos);
+					index.add(stem.stem(word).toString(), filename, ++pos);
 				}
 			}
 		}
+	}
+	
+	/**
+	 * Thread version of traverse
+	 * 
+	 * @see #buildWords(Path, InvertedIndex)
+	 * 
+	 * @throws IOException
+	 */
+	public static void traverse(Path path, InvertedIndex index, int threads) throws IOException {
+		WorkQueue queue = new WorkQueue(threads);
+		
+		for (Path file : TextFileFinder.traverse(path)) {
+			queue.execute(new Builder(file, index));
+		}
+	}
+	
+	/**
+	 * Runnable builder class
+	 * @author Andrew
+	 *
+	 */
+	public static class Builder implements Runnable {
+
+		InvertedIndex index;
+		Path file;
+		
+		public Builder(Path file, InvertedIndex index) {
+			this.index = index;
+			this.file = file;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				synchronized(index) {
+					IndexBuilder.getWords(file, index);
+				}
+			} catch (IOException e) {
+				System.err.println("Cannot make index from file " + file);
+			}
+		}	
 	}
 }
---
+++ src/ThreadSafeInvertedIndex.java
@@ -0,0 +1,152 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.TreeMap;
+
+/**
+ * Thread safe version of inverted index
+ * @author Andrew
+ *
+ */
+public class ThreadSafeInvertedIndex extends InvertedIndex {
+	
+	private ReadWriteLock lock;
+	
+	public ThreadSafeInvertedIndex() {
+		super();
+		lock = new ReadWriteLock();
+	}
+	
+	@Override
+	public void add(String word, String file, int pos) {
+		lock.lockReadWrite();
+		
+		try {
+			super.add(word, file, pos);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public boolean contains(String word) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.contains(word);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public boolean contains(String word, String location) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.contains(word, location);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public boolean contains(String word, String location, int position) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.contains(word, location, position);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public int words() {
+		lock.lockReadWrite();
+		
+		try {
+			return super.words();
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public int locations(String word) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.locations(word);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public int positions(String word, String file) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.positions(word, file);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public void toJSON(Path path) throws IOException {
+		lock.lockReadWrite();
+		
+		try {
+			super.toJSON(path);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public void locationtoJSON(Path locIndex) throws IOException {
+		lock.lockReadWrite();
+		
+		try {
+			super.locationtoJSON(locIndex);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public ArrayList<Result> exactSearch(Collection<String> queries) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.exactSearch(queries);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public ArrayList<Result> partialSearch(Collection<String> queries) {
+		lock.lockReadWrite();
+		
+		try {
+			return super.partialSearch(queries);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+	
+	@Override
+	public void searchHelper(String query, ArrayList<Result> resultList, TreeMap<String, Result> lookup) {
+		lock.lockReadWrite();
+		
+		try {
+			super.searchHelper(query, resultList, lookup);
+		} finally {
+			lock.unlockReadWrite();
+		}
+	}
+}
